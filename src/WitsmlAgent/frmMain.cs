748cd608e7bdce2b6bd72182a7a47a560e4cb043
Mohammad Mehrnia <qermezkon@gmail.com>
2023-08-01 11:20:15 +0330

    -

---
+++ src/WitsmlAgent/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WitsmlAgent.WitsmlStoreServiceReference;
+
+namespace WitsmlAgent
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            WitsmlStoreClient client = new WitsmlStoreClient();
+            string out1, out2;
+            client.ClientCredentials.UserName.UserName = "administrator";
+            client.ClientCredentials.UserName.Password = "Kabinet95##@";
+            client.WMLS_GetCap(string.Empty, out out1, out out2);
+            client.Close();
+        }
+    }
+}
---
+++ src/WitsmlAgentCore/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ServiceModel;
+using WitsmlStoreServiceReference;
+
+Console.WriteLine("Hello, World!");
+BasicHttpsBinding binding = new BasicHttpsBinding();
+binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+EndpointAddress endpoint = new EndpointAddress("https://witsserver.oxinpetro.com/WitsmlStore.svc");
+
+WitsmlStoreClient client = new WitsmlStoreClient(binding, endpoint);
+string out1, out2;
+
+client.ClientCredentials.UserName.UserName = "administrator";
+client.ClientCredentials.UserName.Password = "Kabinet95##@";
+
+client.WMLS_GetCap(string.Empty, out out1, out out2);
+client.Close();
