6dd1a26c05b62dbee1a1b1bab68fd2bd289b5a72
Mohammad Mehrnia <qermezkon@gmail.com>
2023-08-01 11:25:13 +0330

    -

--- src/WitsmlAgentCore/Program.cs
+++ src/WitsmlAgentCore/Program.cs
@@ -2,16 +2,17 @@
 using System.ServiceModel;
 using WitsmlStoreServiceReference;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting...");
 BasicHttpsBinding binding = new BasicHttpsBinding();
 binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-EndpointAddress endpoint = new EndpointAddress("https://witsserver.oxinpetro.com/WitsmlStore.svc");
+EndpointAddress endpoint = new("https://witsserver.oxinpetro.com/WitsmlStore.svc");
 
-WitsmlStoreClient client = new WitsmlStoreClient(binding, endpoint);
+WitsmlStoreClient client = new(binding, endpoint);
 string out1, out2;
 
 client.ClientCredentials.UserName.UserName = "administrator";
 client.ClientCredentials.UserName.Password = "Kabinet95##@";
 
 client.WMLS_GetCap(string.Empty, out out1, out out2);
+
 client.Close();
